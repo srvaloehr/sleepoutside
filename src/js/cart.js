a1a8ef4d82c620bd7be037c12472e0fcde4e7c0a
stephen <your@email.com>
2026-05-23 14:46:02 -0700

    remove cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,14 @@
-import { getLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 
 function renderCartContents() {
   const cartItems = getLocalStorage('so-cart');
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector('.product-list').innerHTML = htmlItems.join('');
+
+  //remove listen
+  document.querySelectorAll('.cart-card__remove') .forEach((btn) =>{ 
+    btn.addEventListener('click', removeFromCart); 
+  });
 
   if (cartItems.length > 0) {
     const total = cartItems.reduce((sum, item) => sum + item.FinalPrice, 0);
@@ -14,6 +19,13 @@ function renderCartContents() {
   }
 }
 
+function removeFromCart(e) {
+  const id = e.target.dataset.id; 
+  const cartItems = getLocalStorage('so-cart') || [];
+  const updatedCart = cartItems.filter((item) => item.Id !== id);
+  setLocalStorage('so-cart', updatedCart);
+  renderCartContents();
+}
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
@@ -29,6 +41,7 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <span class="cart-card__remove" data-id="${item.Id}">X<span>
 </li>`;
 
   return newItem;
