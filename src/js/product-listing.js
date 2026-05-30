aef07523926b136dfcf0bc9faf2448c54eeebbf1
stephen <your@email.com>
2026-05-30 11:20:46 -0700

    name changes

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -5,9 +5,16 @@ import { loadHeaderFooter, getParam } from "./utils.mjs";
 loadHeaderFooter();
 
 const category = getParam("category");
+const categoryNames = {
+  tents: "Tents",
+  backpacks: "Backpacks",
+  "sleeping-bags": "Sleeping Bags",
+  hammocks: "Hammocks",
+};
+
 const dataSource = new ProductData();
 const listElement = document.querySelector(".product-list");
 const titleElement = document.querySelector(".product-list-title");
-titleElement.textContent = `Top Products: ${category}`;
+titleElement.textContent = `Top Products: ${categoryNames[category]}`;
 const myList = new ProductList(category, dataSource, listElement);
 myList.init();
