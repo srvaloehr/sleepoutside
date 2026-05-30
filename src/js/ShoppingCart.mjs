9f576a3c82764fbf1829aa6efa3d1bc63edaf350
stephen <your@email.com>
2026-05-30 11:08:57 -0700

    inventiory api

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -3,7 +3,7 @@ import { getLocalStorage, setLocalStorage } from './utils.mjs';
 function cartItemTemplate(item) {
   return `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
-    <img src="${item.Image}" alt="${item.Name}" />
+    <img src="${item.Images.PrimaryMedium}" alt="${item.Name}" />
   </a>
   <a href="#">
     <h2 class="card__name">${item.Name}</h2>
